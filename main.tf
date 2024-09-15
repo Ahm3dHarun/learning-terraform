ced03648825b0da43717011344b02fd8c7aaddf8
Ahm3dHarun <165212762+Ahm3dHarun@users.noreply.github.com>
2024-09-15 15:25:50 -0500

    x

--- main.tf
+++ main.tf
@@ -66,13 +66,13 @@ module "web-alb" {
     }
   }
 
-  listeners = [
-    {
+  listeners = {
+    http_tcp_listeners = {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0 
     }
-  ]
+  }
 
   tags = {
     Environment = "Dev"
